699341f372c192135ede598221ad5221f73232e8
Jonathan Demme <j.b.demme@gmail.com>
2025-12-21 22:24:54 +0100

    Exercise 2.5: courseinfo seperate course module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,34 +1,4 @@
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = ({parts}) => (
-  <div>
-    {parts.map(part => 
-      <Part key={part.id} part={part}/>
-    )}
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => <p><b>total of exercises {props.total}</b></p>
-
-const Course = ({course}) => (
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total 
-      total={
-        course.parts.reduce((sum, part) => sum + part.exercises, 0)
-      }
-    />
-  </div>
-)
-
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
