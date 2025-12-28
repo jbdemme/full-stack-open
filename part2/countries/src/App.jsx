d5c7a3fd2514f7c1ad32af1ac22772e3de8b5dd3
Jonathan Demme <j.b.demme@gmail.com>
2025-12-28 13:27:56 +0100

    Countries: Refactor country component

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -2,52 +2,47 @@ import { useState } from 'react'
 import axios from 'axios'
 import { useEffect } from 'react'
 
+const Country = ({country}) => {
+  console.log(country)
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
 
-const SearchResults = ({names, onClick}) => {
-  const [country, setCountry] = useState(null)
-  if (names.length == 0) {
+      <div>Capital {country.capital[0]}</div>
+      <div>Area {country.area}</div>
+
+      <h2>Languages</h2>
+      <ul>
+        {Object.entries(country.languages).map(([abbreviation, language]) =>
+          <li key={abbreviation}>{language}</li>
+        )}
+      </ul>
+
+      <img src={country.flags.png} alt={country.flags.alt}/>
+    </div>
+  )
+}
+
+const SearchResults = ({countries, onClick}) => {
+  if (countries.length == 0) {
     return (<></>)
-  }
-  
-  if (names.length > 10) {
+  } else if (countries.length > 10) {
     return (<div>Too many matches, specify another filter</div>)
-  } else if (names.length > 1) {
+  } else if (countries.length > 1) {
     return (
       <>
-        {names.map((name, i) => 
-          <div key={i}>
-            {name} &nbsp;
-            <button className={name} onClick={onClick}>Show</button>
+        {countries.map(country => 
+          <div key={country.cca2}>
+            {country.name.common} &nbsp;
+            <button onClick={() => onClick(country)}>
+              Show
+            </button>
           </div>)
         }
       </>
     )
   } else {
-    if (country && country.name.common == names[0]) {
-      return (
-        <div>
-          <h1>{country.name.common}</h1>
-
-          <div>Capital {country.capital[0]}</div>
-          <div>Area {country.area}</div>
-
-          <h2>Languages</h2>
-          <ul>
-            {Object.values(country.languages).map(((language, i) =>
-              <li key={i}>{language}</li>
-            ))}
-          </ul>
-
-          <img src={country.flags.png} alt={country.flags.alt}/>
-        </div>
-      )
-    } else {
-      axios
-        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${names[0]}`)
-        .then(response =>
-          setCountry(response.data)
-        )
-    }
+    return (<Country country={countries[0]}/>)
   }
 }
 
@@ -55,7 +50,6 @@ const App = () => {
   const [value, setValue] = useState('')
   const [countryData, setCountryData] = useState([])
   const [matchingCountries, setMatchingCountries] = useState([])
-  const [country, setCountry] = useState(null)
 
   useEffect(() => {
     console.log('get country data')
@@ -69,32 +63,23 @@ const App = () => {
   const handleChange = (event) => {
     const query = event.target.value
     setValue(query)
-    const countryNames = countryData.map(c => c.name.common)
-    const matchingNames = countryNames.filter(name =>
-      name.toLowerCase().includes(query.toLowerCase()))
-    setMatchingCountries(matchingNames);
+    setMatchingCountries(countryData.filter(c => {
+      const name = c.name.common
+      return name.toLowerCase().includes(query.toLowerCase())
+    }));
   }
 
-  const handleClick = (event) => {
-    console.log(event.target.className)
-    setMatchingCountries([event.target.className])
+  const handleClick = (country) => {
+    setMatchingCountries([country])
   }
 
-  const renderCountry = (name) => {
-    axios
-      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
-      .then(response => {
-        console.log(response.data)
-        setCountry(response.data)
-      })
-  }
   
   return(
     <div>
       <form>
         find countries<input value={value} onChange={handleChange}/>
       </form>
-      <SearchResults names={matchingCountries} onClick={handleClick}/>
+      <SearchResults countries={matchingCountries} onClick={handleClick}/>
     </div>
   )
 }
