942e3a1722950469726305e25b1c25094a2a9a6c
Jonathan Demme <j.b.demme@gmail.com>
2026-01-12 17:31:20 +0100

    4.20: tokenExtractor into middleware

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -17,11 +17,13 @@ mongoose.connect(config.MONGODB_URI, { family: 4 })
   })
 
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
+
 
 module.exports = app
--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -3,13 +3,7 @@ const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  console.log('auth:', authorization)
-  if(authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-}
+
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -21,7 +15,7 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const { title, author, url, likes } = request.body
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid'})
   }
