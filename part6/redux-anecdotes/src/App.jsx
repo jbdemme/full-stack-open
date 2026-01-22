f474acbfc53c376572a62db036c6550b5537229b
Jonathan Demme <j.b.demme@gmail.com>
2026-01-22 21:06:16 +0100

    setup redux-anecdotes

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from 'react-redux'
+import { vote } from './reducers/anecdoteReducer'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form>
+        <div>
+          <input />
+        </div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
