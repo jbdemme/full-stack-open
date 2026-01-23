58febb16b69ebd859a3cd778014e756a8dca5905
Jonathan Demme <j.b.demme@gmail.com>
2026-01-24 00:03:20 +0100

    6.11 refactor anecdote reducer and action create into RTKs createSlice

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -16,7 +16,7 @@ const AnecdoteList = () => {
 
   return(
     <div>
-      {anecdotes.sort((a, b) => b.votes - a.votes).map(anecdote => (
+      {anecdotes.toSorted((a, b) => b.votes - a.votes).map(anecdote => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
