28971f306060aabdc41f7c3b30ac0de7f805f6c9
Jonathan Demme <j.b.demme@gmail.com>
2026-01-28 01:41:32 +0100

    convert anecdotes in the list to Links

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -21,7 +21,11 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote => 
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
   </div>
 )
@@ -29,12 +33,12 @@ const AnecdoteList = ({ anecdotes }) => (
 const Anecdote = ({anecdote}) => (
   <div>
     <h2>{anecdote.content} by {anecdote.author}</h2>
-    <div>
+    <p>
       has {anecdote.votes} votes
-    </div>
-    <div>
+    </p>
+    <p>
       for more info see <a href={anecdote.info}>{anecdote.info}</a>
-    </div>
+    </p>
   </div>
 )
 
